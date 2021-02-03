f12f7900f11ad8c7c7eda8a1b80f4d648b06a0be
bky373 <bo373@naver.com>
2021-02-03 13:32:10 +0900

    build : remove lectures directory

---
+++ week-06/react-basic-02/react-app-02/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+//컴포넌트를 호출하여 element에 저장합니다.
+//단, props는 name="Sara"를 제공합니다.
+const element33 = <Welcome33 name="Borahm"/>;
+
+ReactDOM.render(element33, document.getElementById('root'));
+
+serviceWorker.unregister();
